2d1c996e663337d1218b0663291e7c9fcb8ed7a4
karim Ashraf <karimashraf000@gmail.com>
2025-03-10 19:52:28 +0200

    create verfiy code and set-password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,8 +11,10 @@ export const routes: Routes = [
       { path: 'login', title: 'Login', loadComponent: () => import('./core/pages/login/login.component').then(c => c.LoginComponent) },
       { path: 'register', title: 'Register', loadComponent: () => import('./core/pages/register/register.component').then(c => c.RegisterComponent) },
       { path: 'forget-password', title: 'Forget Password', loadComponent: () => import('./core/pages/forget-password/forget-password.component').then(c => c.ForgetPasswordComponent) },
+      { path: 'verify-code', title: 'Verify Code', loadComponent: () => import('./core/pages/verify-code/verify-code.component').then(c => c.VerifyCodeComponent) },
+      { path: 'set-password', title: 'Set New Password', loadComponent: () => import('./core/pages/set-password/set-password.component').then(c => c.SetPasswordComponent) },
     ]
   },
   { path: '', redirectTo: 'auth-layout', pathMatch: 'full' },
-  { path: '**', redirectTo: 'auth-layout' } // Handle 404 or unknown routes
+  { path: '**', redirectTo: 'auth-layout' }
 ];
