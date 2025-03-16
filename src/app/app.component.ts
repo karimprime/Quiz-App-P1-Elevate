bb4137fbd4d8e64fea3b694b0034d7a406fa0686
karim Ashraf <karimashraf000@gmail.com>
2025-03-16 08:26:21 +0200

    fix navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { IStaticMethods } from 'flyonui/flyonui';
+
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 @Component({
   selector: 'app-root',
+  standalone: true, // Mark the component as standalone
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-app-v2';
+
+  private readonly router = inject(Router);
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.initializeFlyonUI();
+      }
+    });
+  }
+
+  private initializeFlyonUI(): void {
+    if (typeof window !== 'undefined' && window.HSStaticMethods) {
+      window.HSStaticMethods.autoInit();
+    }
+  }
 }
--- src/app/core/layouts/auth-layout/components/auth-navbar/auth-navbar.component.ts
+++ src/app/core/layouts/auth-layout/components/auth-navbar/auth-navbar.component.ts
@@ -1,39 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-auth-navbar',
-  imports: [RouterLink, RouterLinkActive, CommonModule],
+  imports: [RouterLink , RouterLinkActive],
   templateUrl: './auth-navbar.component.html',
   styleUrl: './auth-navbar.component.scss'
 })
 export class AuthNavbarComponent {
-  isNavOpen = false;
-  isDropdownOpen = false;
 
-  // Toggle mobile navigation menu
-  toggleNav(): void {
-    this.isNavOpen = !this.isNavOpen;
-  }
-
-  // Toggle language dropdown
-  toggleDropdown(event: Event): void {
-    event.stopPropagation();
-    this.isDropdownOpen = !this.isDropdownOpen;
-  }
-
-  // Handle language change
-  changeLanguage(language: string): void {
-    console.log(`Language changed to ${language}`);
-    this.isDropdownOpen = false;
-  }
-
-  // Close dropdown when clicking outside
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: Event): void {
-    if (!(event.target as HTMLElement).closest('.dropdown')) {
-      this.isDropdownOpen = false;
-    }
-  }
 }
